6ecfa9639e6cb868879030ccaf4bc5f4415f3cb0
Wendy Horvat <57775231+LigerExpress@users.noreply.github.com>
2021-04-10 15:55:17 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace first_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("You can press a key to continue.");
+            string x;
+            string b;
+            x = "a";
+
+            b = Console.ReadLine();
+            if (b == x)
+            { Console.WriteLine("move on"); 
+            }
+               
+            else Console.WriteLine("denied");
+        }
+    }
+}
